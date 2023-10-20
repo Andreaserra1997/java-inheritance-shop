ffb39fe7c47706b81d8fce7490eda212e79c513f
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-10-20 15:55:33 +0200

    add Main & Prodotto

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,9 @@
+package org.lessons.java.shop;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Prodotto product1 = new Prodotto("Telefono", "Bellissimo", 199, 22);
+        System.out.println(product1.getName());
+    }
+}
---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,52 @@
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Prodotto {
+    // ATTRIBUTI
+    private int code;
+    private String name;
+    private String description;
+    private int price;
+    private int iva;
+
+    Random codRandom = new Random();
+
+    //COSTRUTTORI
+
+    public Prodotto(String name, String description, int price, int iva) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva= iva;
+        code = codRandom.nextInt(1, 100);
+    }
+
+    //METODI
+
+    public String getNameComplete() {
+        return name + description;
+    }
+    public int getPriceIva() {
+        return price - (price * iva / 100);
+    }
+
+    //GETTER E SETTER
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getIva() {
+        return iva;
+    }
+
+}
